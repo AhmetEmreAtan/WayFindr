156491075e0f9d7cd47a845dfa54e9092a0b1833
Ahmet Emre Atan <ahmet_emrea@hotmail.com>
2023-08-18 15:23:15 +0300

    App Nav Bar Commit

---
+++ app/src/main/java/com/example/wayfindr/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.example.wayfindr
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.wayfindr.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+
+    private lateinit var binding : ActivityMainBinding
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(Home())
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+
+            when(it.itemId){
+
+                R.id.home -> replaceFragment(Home())
+                R.id.places -> replaceFragment(Places())
+                R.id.profile -> replaceFragment(Profile())
+
+                else -> {
+
+                }
+
+            }
+
+            true
+
+        }
+
+    }
+
+
+    private fun replaceFragment(fragment: Fragment) {
+
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout, fragment)
+        fragmentTransaction.commit()
+
+    }
+}
