32c42bbe47a45e47802e8e343fee92deb5b1f4de
haticesule <123954023+haticesule@users.noreply.github.com>
2023-09-03 18:10:21 +0300

    Dont Change.

--- app/src/main/java/com/example/wayfindr/Login.kt
+++ app/src/main/java/com/example/wayfindr/Login.kt
@@ -1,28 +1,21 @@
-package com.example.giris
+package com.example.wayfindr
 
-import android.content.Intent
-import android.graphics.drawable.ColorDrawable
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Patterns
-import android.widget.Button
 import android.widget.EditText
-import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
-import com.example.giris.databinding.ActivityLoginBinding
-import com.google.firebase.auth.FirebaseAuth
+import com.example.wayfindr.databinding.ActivityLoginBinding
 
 class Login : AppCompatActivity() {
 
     private lateinit var binding: ActivityLoginBinding
-    private lateinit var firebaseAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding  = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        firebaseAuth = FirebaseAuth.getInstance()
 
         binding.forgetPassword.setOnClickListener {
             val builder = AlertDialog.Builder(this)
@@ -31,52 +24,11 @@ class Login : AppCompatActivity() {
 
             builder.setView(view)
             val dialog = builder.create()
-
-            view.findViewById<Button>(R.id.btnReset).setOnClickListener {
-                compareEmail(userEmail)
-                dialog.dismiss()
-            }
-            view.findViewById<Button>(R.id.btnCancel).setOnClickListener {
-                dialog.dismiss()
-            }
-            if(dialog.window != null){
-                dialog.window!!.setBackgroundDrawable(ColorDrawable(0))
-            }
-            dialog.show()
         }
         binding.singIn.setOnClickListener {
             val email = binding.eMail.text.toString()
             val password = binding.passwords.text.toString()
 
-            if(email.isNotEmpty() && password.isNotEmpty()){
-                firebaseAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener{
-                    if(it.isSuccessful){
-                        val intent = Intent(this, MainActivity::class.java)
-                        startActivity(intent)
-                    }
-                    else{
-                        Toast.makeText(this, "Hesap mevcut değil",Toast.LENGTH_SHORT).show()
-
-                    }
-                }
-             }
-            else{
-                Toast.makeText(this, "Lütfen tüm boş alanları doldurduğunuza emin olun",Toast.LENGTH_SHORT).show()
-
-            }
-        }
-    }
-    private fun compareEmail(email: EditText) {
-        if(email.text.toString().isEmpty()){
-            return
-        }
-        if (!Patterns.EMAIL_ADDRESS.matcher(email.text.toString()).matches()){
-            return
-        }
-        firebaseAuth.sendPasswordResetEmail(email.text.toString()).addOnCompleteListener { task ->
-            if(task.isSuccessful){
-                Toast.makeText(this,"E Postanızı kontrol edin.", Toast.LENGTH_SHORT).show()
-            }
         }
     }
 }
--- app/src/main/java/com/example/wayfindr/Register.kt
+++ app/src/main/java/com/example/wayfindr/Register.kt
@@ -1,57 +1,23 @@
-package com.example.giris
+package com.example.wayfindr
 
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
-import com.example.giris.databinding.ActivityLoginBinding
-import com.example.giris.databinding.ActivityRegisterBinding
-import com.google.firebase.auth.FirebaseAuth
+import com.example.wayfindr.databinding.ActivityRegisterBinding
 
 class Register : AppCompatActivity() {
 
     private lateinit var binding: ActivityRegisterBinding
-    private lateinit var firebaseAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        firebaseAuth = FirebaseAuth.getInstance()
 
         fun showToast(message: String) {
             Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
-        }
-        binding.singUp.setOnClickListener{
-
-            val name =binding.name.text.toString()
-            val date = binding.date.text.toString()
-            val gender = binding.gender.text.toString()
-            val email = binding.eMail2.text.toString()
-            val passwords = binding.passwords2.text.toString()
-            val confirmPasswords = binding.confirmPasswords2.text.toString()
-
-            if(name.isNotEmpty() || date.isNotEmpty() || gender.isNotEmpty() || email.isNotEmpty() || passwords.isNotEmpty() || confirmPasswords.isNotEmpty()){
-                if(passwords == confirmPasswords){
-
-                    firebaseAuth.createUserWithEmailAndPassword(email,passwords).addOnCompleteListener{
-                        if (it.isSuccessful){
-                            val intent = Intent(this, MainActivity::class.java)
-                            startActivity(intent)
-                        }
-                        else{
-                            Toast.makeText(this,it.exception.toString(), Toast.LENGTH_SHORT).show()
-                        }
-                    }
-                }
-                else{
-                    Toast.makeText(this,"Şifre giriniz", Toast.LENGTH_SHORT).show()
-                }
-            }
-            else{
-                showToast("Lütfen tüm boş alanları doldurduğunuza emin olun.")
-            }
         }
 
         binding.notSingUp.setOnClickListener {
