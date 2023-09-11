4b3851f5a32761f0abaf0094f27499598e05e6e0
Ahmet Emre Atan <ahmet_emrea@hotmail.com>
2023-09-11 19:31:41 +0300

    Profile error has solved.

--- app/src/main/java/com/example/wayfindr/Profile.kt
+++ app/src/main/java/com/example/wayfindr/Profile.kt
@@ -47,7 +47,7 @@ class Profile : Fragment() {
 
         val notSignupTxt = view.findViewById<TextView>(R.id.notSingUp)
         notSignupTxt.setOnClickListener {
-            val intent = Intent(requireActivity(), Home::class.java)
+            val intent = Intent(requireActivity(), Profile_page::class.java)
             startActivity(intent)
         }
         val letLoginTxt = view.findViewById<TextView>(R.id.letLogIn)
