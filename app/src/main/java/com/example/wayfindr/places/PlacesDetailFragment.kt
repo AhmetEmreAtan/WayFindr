098fc1bf4f97c081f40a17e820ec81519da0cb5c
kadersutlu <sutlukader@gmail.com>
2024-07-09 16:00:56 +0300

    adding place comments and listing updated

--- app/src/main/java/com/example/wayfindr/places/PlacesDetailFragment.kt
+++ app/src/main/java/com/example/wayfindr/places/PlacesDetailFragment.kt
@@ -18,23 +18,20 @@ import com.example.wayfindr.databinding.FragmentPlacesDetailBinding
 import com.example.wayfindr.home.Navigation
 import com.google.android.gms.maps.model.LatLng
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.database.ValueEventListener
 import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.firestore.SetOptions
 
 class PlacesDetailFragment : Fragment() {
 
     private var _binding: FragmentPlacesDetailBinding? = null
     private val binding get() = _binding!!
-    private val db = FirebaseFirestore.getInstance()
+
     private val auth = FirebaseAuth.getInstance()
     private val currentUser = auth.currentUser
     private val userId = currentUser?.uid
 
     private val favoriteRepository = FavoriteRepository()
+    private lateinit var commentList: MutableList<CommentPlacesModel>
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -55,184 +52,153 @@ class PlacesDetailFragment : Fragment() {
         val selectedPlace = arguments?.getSerializable("selectedPlace") as? PlaceModel
         val selectedPlaceId = selectedPlace?.placeId
 
-        var placesName = binding.placesName
-        var placesImage = binding.placesImage
-        var placesDetails = binding.placesDetails
-        var placesOpeningHours = binding.placesOpeningHours
-        var placesPrice = binding.placesPrice
-        var placesAddress = binding.placesAddress
-        var commentDetail = binding.commentDetail
-        var addedComment = binding.commentSend
+        setupUI(view, selectedPlace)
+        setupRecyclerView()
+        fetchPlaceDetails(selectedPlaceId)
+        fetchComments(selectedPlaceId)
 
-        if (selectedPlace != null) {
-            val placeId = selectedPlace.placeId
-
-            if (placeId.isNotBlank()) {
-                db.collection("places")
-                    .document(placeId)
-                    .get()
-                    .addOnSuccessListener { document ->
-                        if (document != null && document.exists()) {
-
-                            val placeModel = document.toObject(PlaceModel::class.java)
-
-                            placesName?.text = placeModel?.placeName
-                            placesAddress?.text = placeModel?.placeAddress
-                            placesOpeningHours?.text = placeModel?.placeOpeningHours
-                            placesPrice?.text = placeModel?.placePrice
-                            placesDetails?.text = placeModel?.placeDetails
-                            Glide.with(view)
-                                .load(placeModel?.placeImage)
-                                .placeholder(R.drawable.placeholder_image)
-                                .error(R.drawable.error_image)
-                                .centerCrop()
-                                .into(placesImage!!)
-
-                            println(placeModel)
-                        } else {
-                            println("Belge bulunamadı")
-                        }
-                    }
-                    .addOnFailureListener { exception ->
-                        println("Firestore veri çekme hatası: $exception")
-                    }
-            } else {
-                println("Belge ID'si boş")
-            }
-        } else {
-            println("Belge ID'si null")
-        }
-
-        val commentList = mutableListOf<CommentPlacesModel>()
-        val layoutManager = LinearLayoutManager(requireContext())
-        binding.commentRecyclerView.layoutManager = layoutManager
-        val commentAdapter = CommentPlacesAdapter(commentList)
-        binding.commentRecyclerView.adapter = commentAdapter
-
-        if (selectedPlace != null) {
-            val placeId = selectedPlace.placeId
-
-            if (!placeId.isNullOrBlank()) {
-                val placeRef =
-                    FirebaseFirestore.getInstance().collection("places").document(placeId)
-
-                placeRef.collection("comments")
-                    .get()
-                    .addOnSuccessListener { commentsQuerySnapshot ->
-                        commentList.clear()
-
-                        for (commentDocument in commentsQuerySnapshot.documents) {
-                            val commentText = commentDocument.getString("commentText")
-
-                            val userMap = commentDocument.get("user") as Map<String, Any>?
-                            val userModel = UserData(
-                                userId = userMap?.get("id") as String?,
-                                username = userMap?.get("userName") as String?,
-                                firstName = userMap?.get("name") as String?,
-                                profileImageUrl = userMap?.get("profileImage") as String?
-                            )
-
-                            val commentModel = CommentPlacesModel(
-                                commentText = commentText,
-                                user = userModel
-                            )
-
-                            commentList.add(commentModel)
-                        }
-
-                        commentAdapter.notifyDataSetChanged()
-                    }
-                    .addOnFailureListener { e ->
-                        Log.e("asas", "Firestore'dan comment verilerini alma hatası: $e")
-                    }
-            } else {
-                Log.i("asas", "Yer ID'si boş")
-            }
-        } else {
-            Log.i("asas", "Belge ID'si null")
-        }
-
-
-        addedComment.setOnClickListener {
-            val commentText = commentDetail.text.toString()
-            val placeId = selectedPlace?.placeId
-            val user = auth.currentUser
-
-            if (user != null && !placeId.isNullOrBlank()) {
-                val userId = user.uid
-                val userRef = FirebaseFirestore.getInstance().collection("users").document(userId)
-                val realtimeDatabaseRef =
-                    FirebaseDatabase.getInstance().getReference("users").child(userId)
-                val userData = hashMapOf<String, Any>()
-
-                realtimeDatabaseRef.addListenerForSingleValueEvent(object : ValueEventListener {
-                    override fun onDataChange(dataSnapshot: DataSnapshot) {
-                        val realtimeUserData = dataSnapshot.value as Map<String, Any>?
-
-                        if (realtimeUserData != null) {
-                            userData.putAll(realtimeUserData as Map<out String, Any>)
-                        }
-
-                        userRef.set(userData, SetOptions.merge())
-                            .addOnSuccessListener {
-                                Log.i("asas", "Kullanıcı belgesi güncellendi: $userData")
-
-                                val commentData = hashMapOf(
-                                    "commentText" to commentText,
-                                    "user" to userData
-                                )
-
-                                val placeRef = FirebaseFirestore.getInstance().collection("places")
-                                    .document(placeId)
-                                placeRef.collection("comments").add(commentData)
-                                    .addOnSuccessListener { documentReference ->
-                                        Toast.makeText(
-                                            requireContext(),
-                                            "Yorumunuz başarıyla eklendi",
-                                            Toast.LENGTH_SHORT
-                                        ).show()
-                                        Log.i("asas", "Yorum eklendi ${documentReference.id}")
-
-                                        val newComment = CommentPlacesModel(
-                                            commentText,
-                                            UserData(
-                                                userId = user.uid,
-                                                firstName = user.displayName,
-                                                email = user.email,
-                                                username = user.displayName,
-                                                profileImageUrl = user.photoUrl.toString()
-                                            )
-                                        )
-
-                                        commentList.add(newComment)
-                                        commentAdapter.notifyDataSetChanged()
-
-                                        layoutManager.scrollToPositionWithOffset(0, 0)
-                                    }
-                                    .addOnFailureListener { e ->
-                                        Toast.makeText(
-                                            requireContext(),
-                                            "Yorumunuz eklenmedi",
-                                            Toast.LENGTH_SHORT
-                                        ).show()
-                                        Log.e("sa", "yorum ekleme hatası $e")
-                                    }
-                            }
-                            .addOnFailureListener { e ->
-                                Log.e("asas", "Kullanıcı belgesini güncelleme hatası: $e")
-                            }
-                    }
-
-                    override fun onCancelled(databaseError: DatabaseError) {
-                        Log.e("asas", "Firebase Realtime Database hata: $databaseError")
-                    }
-                })
-            } else {
-                Log.i("asas", "Kullanıcı girişi yapmamış veya Yer ID'si boş veya null")
-            }
+        binding.commentSend.setOnClickListener {
+            addComment(selectedPlaceId)
         }
 
         addRemoveFavorites(selectedPlaceId, selectedPlace)
+    }
+
+    private fun setupUI(view: View, selectedPlace: PlaceModel?) {
+        binding.apply {
+            placesName.text = selectedPlace?.placeName ?: ""
+            placesAddress.text = selectedPlace?.placeAddress ?: ""
+            placesOpeningHours.text = selectedPlace?.placeOpeningHours ?: ""
+            placesPrice.text = selectedPlace?.placePrice ?: ""
+            placesDetails.text = selectedPlace?.placeDetails ?: ""
+            Glide.with(view)
+                .load(selectedPlace?.placeImage)
+                .placeholder(R.drawable.placeholder_image)
+                .error(R.drawable.error_image)
+                .centerCrop()
+                .into(placesImage)
+        }
+    }
+
+    private fun setupRecyclerView() {
+        commentList = mutableListOf()
+        binding.commentRecyclerView.apply {
+            layoutManager = LinearLayoutManager(requireContext())
+            adapter = CommentPlacesAdapter(commentList)
+        }
+    }
+
+    private fun fetchPlaceDetails(placeId: String?) {
+        if (placeId.isNullOrBlank()) {
+            return
+        }
+
+        FirebaseFirestore.getInstance().collection("places")
+            .document(placeId)
+            .get()
+            .addOnSuccessListener { document ->
+                if (document != null && document.exists()) {
+                    val placeModel = document.toObject(PlaceModel::class.java)
+                    setupUI(requireView(), placeModel)
+                }
+            }
+    }
+
+    private fun fetchComments(placeId: String?) {
+        if (placeId.isNullOrBlank()) {
+            return
+        }
+
+        FirebaseFirestore.getInstance().collection("places")
+            .document(placeId)
+            .collection("comments")
+            .get()
+            .addOnSuccessListener { commentsQuerySnapshot ->
+                commentList.clear()
+                for (commentDocument in commentsQuerySnapshot.documents) {
+                    val commentText = commentDocument.getString("commentText")
+                    val userMap = commentDocument.get("user") as? Map<String, String>
+                    val userId = userMap?.get("userId")
+                    if (!userId.isNullOrBlank()) {
+                        fetchUserDetails(userId, commentText)
+                    }
+                }
+            }
+    }
+
+    private fun fetchUserDetails(userId: String, commentText: String?) {
+        FirebaseFirestore.getInstance().collection("users")
+            .document(userId)
+            .get()
+            .addOnSuccessListener { userDocument ->
+                if (userDocument != null && userDocument.exists()) {
+                    val userModel = userDocument.toObject(UserData::class.java)
+                    if (userModel != null) {
+                        val commentModel = CommentPlacesModel(
+                            commentText = commentText,
+                            user = userModel
+                        )
+                        commentList.add(commentModel)
+                        binding.commentRecyclerView.adapter?.notifyDataSetChanged()
+                    }
+                }
+            }
+    }
+
+    private fun addComment(placeId: String?) {
+        val commentText = binding.commentDetail.text.toString()
+        val user = auth.currentUser
+
+        if (user != null && !placeId.isNullOrBlank()) {
+            val userId = user.uid
+            val userRef = FirebaseFirestore.getInstance().collection("users").document(userId)
+
+            userRef.get()
+                .addOnSuccessListener { documentSnapshot ->
+                    if (documentSnapshot.exists()) {
+                        val userFirestoreData = documentSnapshot.toObject(UserData::class.java)
+                        if (userFirestoreData != null) {
+                            val userData = mapOf(
+                                "userId" to userId,
+                                "username" to userFirestoreData.username.orEmpty(),
+                                "firstName" to userFirestoreData.firstName.orEmpty(),
+                                "profileImageUrl" to userFirestoreData.profileImageUrl.orEmpty()
+                            )
+                            val commentData = mapOf(
+                                "commentText" to commentText,
+                                "user" to userData
+                            )
+
+                            FirebaseFirestore.getInstance().collection("places")
+                                .document(placeId)
+                                .collection("comments")
+                                .add(commentData)
+                                .addOnSuccessListener { documentReference ->
+                                    Toast.makeText(
+                                        requireContext(),
+                                        "Your comment was added successfully",
+                                        Toast.LENGTH_SHORT
+                                    ).show()
+
+                                    val newComment = CommentPlacesModel(
+                                        commentText,
+                                        UserData(
+                                            userId = userId,
+                                            firstName = userFirestoreData.firstName,
+                                            username = userFirestoreData.username,
+                                            profileImageUrl = userFirestoreData.profileImageUrl
+                                        )
+                                    )
+
+                                    commentList.add(newComment)
+                                    binding.commentRecyclerView.adapter?.notifyDataSetChanged()
+                                    binding.commentRecyclerView.layoutManager?.scrollToPosition(0)
+                                }
+
+                        }
+                    }
+                }
+        }
     }
 
     private fun addRemoveFavorites(
